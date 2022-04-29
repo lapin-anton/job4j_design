0aa1064bf6bb6213764cc36f3dc1c3a32e682ec1
anton-lapin <lapin_antoine@mail.ru>
2022-04-29 09:11:54 +0300

    5. Очередь на двух стеках [#160 #294747]

--- src/main/java/ru/job4j/collection/SimpleQueue.java
+++ src/main/java/ru/job4j/collection/SimpleQueue.java
@@ -5,25 +5,28 @@ import java.util.NoSuchElementException;
 public class SimpleQueue<T> {
     private final SimpleStack<T> in = new SimpleStack<>();
     private final SimpleStack<T> out = new SimpleStack<>();
+    private int size;
 
     public T poll() {
+        if (size == 0) {
+            throw new NoSuchElementException();
+        }
+        size--;
         return out.pop();
     }
 
     public void push(T value) {
-        copyElements(out, in);
+        if (size > 0) {
+            copyElements(out, in);
+        }
         in.push(value);
+        size++;
         copyElements(in, out);
     }
 
     private void copyElements(SimpleStack<T> src, SimpleStack<T> target) {
-        while (true) {
-            try {
-                T el = src.pop();
-                target.push(el);
-            } catch (NoSuchElementException e) {
-                break;
-            }
+        for (int i = 0; i < size; i++) {
+            target.push(src.pop());
         }
     }
 }
