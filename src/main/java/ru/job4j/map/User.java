8244958340f95c6fb061d1e373d728be493bcf2e
anton-lapin <lapin_antoine@mail.ru>
2022-04-30 20:51:02 +0300

    2. Без переопределения equals и hashCode [#1005]

--- src/main/java/ru/job4j/map/User.java
+++ src/main/java/ru/job4j/map/User.java
@@ -1,6 +1,6 @@
 package ru.job4j.map;
 
-import java.util.Calendar;
+import java.util.*;
 
 public class User {
     private String name;
@@ -11,5 +11,16 @@ public class User {
         this.name = name;
         this.children = children;
         this.birthday = birthday;
+    }
+
+    public static void main(String[] args) {
+        User userOne = new User("one", 0, new GregorianCalendar(2000, 1, 1));
+        User userTwo = new User("one", 0, new GregorianCalendar(2000, 1, 1));
+        Map<User, Object> map = new HashMap<>();
+        map.put(userOne, new Object());
+        map.put(userTwo, new Object());
+        for (User key: map.keySet()) {
+            System.out.println(key + " : " + map.get(key));
+        }
     }
 }
