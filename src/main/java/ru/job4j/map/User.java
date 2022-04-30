97ad8d6e2ecb64711399f914b54791c0b23cfc10
anton-lapin <lapin_antoine@mail.ru>
2022-04-30 21:46:07 +0300

    4. Переопределить только equals [#1004]

--- src/main/java/ru/job4j/map/User.java
+++ src/main/java/ru/job4j/map/User.java
@@ -16,8 +16,22 @@ public class User {
     }
 
     @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        User user = (User) o;
+        return children == user.children
+                && Objects.equals(name, user.name)
+                && Objects.equals(birthday, user.birthday);
+    }
+
+    @Override
     public int hashCode() {
-        return Objects.hash(name, children, birthday);
+        return super.hashCode();
     }
 
     public static void main(String[] args) {
@@ -25,6 +39,9 @@ public class User {
                 ZonedDateTime.of(2000, 1, 1, 0, 0, 0, 0, ZoneId.systemDefault()));
         User userOne = new User("one", 0, date);
         User userTwo = new User("one", 0, date);
+        System.out.println(userOne.hashCode());
+        System.out.println(userTwo.hashCode());
+        System.out.println(userOne.equals(userTwo));
         Map<User, Object> map = new HashMap<>();
         map.put(userOne, new Object());
         map.put(userTwo, new Object());
