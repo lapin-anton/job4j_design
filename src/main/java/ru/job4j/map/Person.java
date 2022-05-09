de4dd12f36345c4650bbba9a520a6d24404d4e52
anton-lapin <lapin_antoine@mail.ru>
2022-05-09 22:56:39 +0300

    9.1. Рассказать и продемонстрировать как переопределяют метод hashCode [#9881 #299225]

--- src/main/java/ru/job4j/map/Person.java
+++ src/main/java/ru/job4j/map/Person.java
@@ -30,8 +30,12 @@ public class Person {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         Person person = (Person) o;
         return gender == person.gender
                 && age == person.age
