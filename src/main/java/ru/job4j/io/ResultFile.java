0de925c6e50534ccbc22876ebec337771277371b
anton-lapin <lapin_antoine@mail.ru>
2022-05-12 22:38:59 +0300

    0.1. FileOutputStream. [#252488]

--- src/main/java/ru/job4j/io/ResultFile.java
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -5,7 +5,7 @@ import java.nio.charset.StandardCharsets;
 
 public class ResultFile {
     public static void main(String[] args) {
-        try (FileOutputStream out = new FileOutputStream("result.txt")) {
+        try(FileOutputStream out = new FileOutputStream("result.txt")) {
             for (int i = 1; i < 10; i++) {
                 for (int j = 1; j < 10; j++) {
                     out.write(String.format("%d x %d = %d", i, j, i * j).getBytes(StandardCharsets.UTF_8));
