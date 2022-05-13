4e462af0ca6feaa53d14acae0f11857c0aaa5227
anton-lapin <lapin_antoine@mail.ru>
2022-05-13 22:21:34 +0300

    2. Анализ доступности сервера. [#859 #300567]

--- src/main/java/ru/job4j/io/Analizy.java
+++ src/main/java/ru/job4j/io/Analizy.java
@@ -1,35 +1,28 @@
 package ru.job4j.io;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.regex.Pattern;
 
 public class Analizy {
     public void unavailable(String source, String target) {
-        List<Status> statuses = new ArrayList<>();
-        try (BufferedReader reader = new BufferedReader(new FileReader(source))) {
+        String regex = "^[\\d]{3}\\s([\\d]{2}:){2}[\\d]{2}$";
+        try (BufferedReader reader = new BufferedReader(new FileReader(source));
+             PrintWriter writer = new PrintWriter(new FileOutputStream(target))) {
+            int lastCode = 0;
+            String begin = null;
             while (reader.ready()) {
                 String s = reader.readLine();
-                if (!"".equals(s)) {
+                if (!"".equals(s) && Pattern.matches(regex, s)) {
                     String[] items = s.split("\\s");
                     int code = Integer.parseInt(items[0]);
                     String time = items[1];
-                    statuses.add(new Status(code, time));
+                    if (code >= 400 && code <= 500 && lastCode >= 200 && lastCode <= 300) {
+                        begin = time;
+                    } else if (code >= 200 && code <= 300 && lastCode >= 400 && lastCode <= 500) {
+                        writer.printf("%s;%s;%s", begin, time, System.lineSeparator());
+                    }
+                    lastCode = code;
                 }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        try (PrintWriter writer = new PrintWriter(new FileOutputStream(target))) {
-            int lastCode = 0;
-            String begin = null;
-            for (Status s: statuses) {
-                if (s.getCode() >= 400 && s.getCode() <= 500 && lastCode >= 200 && lastCode <= 300) {
-                    begin = s.getTime();
-                } else if (s.getCode() >= 200 && s.getCode() <= 300 && lastCode >= 400 && lastCode <= 500) {
-                    writer.printf("%s;%s;%s", begin, s.getTime(), System.lineSeparator());
-                }
-                lastCode = s.getCode();
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -41,21 +34,4 @@ public class Analizy {
         analizy.unavailable("source.txt", "unavailable.csv");
     }
 
-    private static class Status {
-        private int code;
-        private String time;
-
-        public Status(int code, String time) {
-            this.code = code;
-            this.time = time;
-        }
-
-        public int getCode() {
-            return code;
-        }
-
-        public String getTime() {
-            return time;
-        }
-    }
 }
