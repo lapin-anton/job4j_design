73f74fac0886ab1686152463d484f90161e04870
anton-lapin <lapin_antoine@mail.ru>
2022-05-14 16:48:34 +0300

    5. Валидация параметров запуска. [#246865]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -6,16 +6,29 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 import java.util.function.Predicate;
+import java.util.regex.Pattern;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get("C:\\projects");
-        search(start, p -> p.toFile().getName().endsWith(".js")).forEach(System.out::println);
+        validateArgs(args);
+        Path start = Paths.get(args[0]);
+        search(start, p -> p.toFile().getName().endsWith(args[1])).forEach(System.out::println);
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
         SearchFiles searcher = new SearchFiles(condition);
         Files.walkFileTree(root, searcher);
         return searcher.getPaths();
+    }
+
+    private static void validateArgs(String[] args) {
+        String extFormat = "^\\.\\w+$";
+        if (args.length < 2) {
+            throw new IllegalArgumentException("Amount of input args is less than 2. "
+                    + "Usage java -jar search.jar ROOT_FOLDER FILE_EXTENSION.");
+        }
+        if (!Pattern.matches(extFormat, args[1])) {
+            throw new IllegalArgumentException(String.format("Argument '%s' has incorrect file extension format", args[1]));
+        }
     }
 }
