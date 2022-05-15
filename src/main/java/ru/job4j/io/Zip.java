00eb608b690cc09f3396698196256ddf070f2d48
anton-lapin <lapin_antoine@mail.ru>
2022-05-15 13:52:17 +0300

    5.2. Архивировать проект [#861 #301130]

--- src/main/java/ru/job4j/io/Zip.java
+++ src/main/java/ru/job4j/io/Zip.java
@@ -4,31 +4,18 @@ import java.io.*;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
-import java.util.stream.Collectors;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 public class Zip {
-    public void packFiles(List<File> sources, File target) {
+    public void packFiles(List<Path> sources, File target) {
         try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
-            for (File file: sources) {
-                zip.putNextEntry(new ZipEntry(file.getPath()));
-                try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(file))) {
+            for (Path path: sources) {
+                zip.putNextEntry(new ZipEntry(path.toString()));
+                try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(path.toFile()))) {
                     zip.write(out.readAllBytes());
                     zip.closeEntry();
                 }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void packSingleFile(File source, File target) {
-        try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
-            zip.putNextEntry(new ZipEntry(source.getPath()));
-            try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(source))) {
-                zip.write(out.readAllBytes());
-                zip.closeEntry();
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -41,8 +28,7 @@ public class Zip {
         String exclude = argsName.get("e");
         String output = argsName.get("o");
         List<Path> paths = Search.search(Paths.get(directory), p -> !p.toFile().getName().endsWith(exclude));
-        List<File> sources = paths.stream().map(Path::toFile).collect(Collectors.toList());
         Zip zip = new Zip();
-        zip.packFiles(sources, new File(output));
+        zip.packFiles(paths, new File(output));
     }
 }
